212744665624d7c880701df6c4a745dba838d3ea
Solitaire <reven777@live.com>
2016-08-28 01:22:40 -0700

    added attributes, race, social status, skills

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -11,22 +11,57 @@ from evennia import DefaultCharacter
 
 class Character(DefaultCharacter):
     """
-    The Character defaults to reimplementing some of base Object's hook methods with the
-    following functionality:
-
-    at_basetype_setup - always assigns the DefaultCmdSet to this object type
-                    (important!)sets locks so character cannot be picked up
-                    and its commands only be called by itself, not anyone else.
-                    (to change things, use at_object_creation() instead).
-    at_after_move - Launches the "look" command after every move.
-    at_post_unpuppet(player) -  when Player disconnects from the Character, we
-                    store the current location in the pre_logout_location Attribute and
-                    move it to a None-location so the "unpuppeted" character
-                    object does not need to stay on grid. Echoes "Player has disconnected" 
-                    to the room.
-    at_pre_puppet - Just before Player re-connects, retrieves the character's
-                    pre_logout_location Attribute and move it back on the grid.
-    at_post_puppet - Echoes "PlayerName has entered the game" to the room.
-
+    Custom SoS-statted character. Todo: implement chargen.
     """
+def at_object_creation(self):
+    "Called only when first created"
+    #race attribute creation
+    self.db.race = "unassigned"
+    #attribute attribute creation
+    self.db.agility = 0
+    self.db.endurance = 0
+    self.db.health = 0
+    self.db.willpower = 0
+    self.db.wit = 0
+    self.db.intelligence = 0
+    #derived attribute creation
+    self.db.adroitness = 0
+    self.db.mobility = 0
+    self.db.carry = 0
+    self.db.toughness = 0
+    self.db.charisma = 0
+    self.db.grit = 0
+    #skill attribute creation
+    self.db.athletics = 0 
+    self.db.chemistry = 0
+    self.db.climbing = 0
+    self.db.cooking = 0
+    self.db.crafting = 0
+    self.db.drill = 0
+    self.db.engineering = 0
+    self.db.gather_information = 0
+    self.db.history = 0
+    self.db.hunting = 0
+    self.db.intimidate = 0
+    self.db.navigation = 0
+    self.db.observation = 0
+    self.db.orate = 0
+    self.db.perform = 0
+    self.db.persuasion = 0
+    self.db.politics = 0
+    self.db.research = 0
+    self.db.riding = 0
+    self.db.sailing = 0
+    self.db.stealth = 0
+    self.db.strategy = 0
+    self.db.subterfuge = 0
+    self.db.surgery = 0
+    self.db.swimming = 0
+    self.db.tactics = 0
+    #social class attribute creation
+    self.db.social_class = "unassigned"
+    #magic attribute creation
+    self.db.magic = 0
+
     pass
+
